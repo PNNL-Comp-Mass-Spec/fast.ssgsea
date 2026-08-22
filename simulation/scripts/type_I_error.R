4fbd046ef6a84060c435ae30af7955132a1565e5
TylerSagendorf <tjsagendorf@gmail.com>
2026-08-22 10:28:53 -0500

    Update type I error scripts
    Tested HPGSEA, ssGSEA2.0, and FGSEA with alpha = 0 and alpha = 1. Merged inter_gene_correlation.R and type_I_error.R.

--- simulation/scripts/type_I_error.R
+++ simulation/scripts/type_I_error.R
@@ -16,207 +16,176 @@ gene_set <- list(
   "set" = genes[seq_len(set_size)]
 )
 
+mu <- rep_len(0, set_size)
 
-# ssGSEA2.0 (alpha = 0) ----
+for (inter_gene_cor in c(0, 0.05)) {
 
-message("ssGSEA2.0 (alpha = 0)...")
-
-pvals_ssgsea2_0 <- map_dbl(seq_len(n_datasets), function(i) {
-  set.seed(i)
-  stats <- rnorm(n_genes)
-  names(stats) <- genes
-
-  ssgsea2(
-    stats = stats,
-    gene_sets = gene_set,
-    alpha = 0,
-    seed = i,
-    time_only = FALSE
-  )[["results"]][["p_value"]]
-}, .progress = TRUE)
-
-
-# ssGSEA2.0 (alpha = 1) ----
-
-message("ssGSEA2.0 (alpha = 1)...")
-
-pvals_ssgsea2_1 <- map_dbl(seq_len(n_datasets), function(i) {
-  set.seed(i)
-  stats <- rnorm(n_genes)
-  names(stats) <- genes
-
-  ssgsea2(
-    stats = stats,
-    gene_sets = gene_set,
-    alpha = 1,
-    seed = i,
-    time_only = FALSE
-  )[["results"]][["p_value"]]
-}, .progress = TRUE)
-
-
-# HPGSEA (alpha = 0) ----
-
-message("HPGSEA (alpha = 0)...")
-
-pvals_hpgsea_0 <- map_dbl(seq_len(n_datasets), function(i) {
-  set.seed(i)
-  stats <- rnorm(n_genes)
-  names(stats) <- genes
-
-  hpgsea(
-    stats = stats,
-    gene_sets = gene_set,
-    alpha = 0,
-    seed = i
-  )[["p_value"]]
-}, .progress = TRUE)
-
-
-# HPGSEA (alpha = 1) ----
-
-message("HPGSEA (alpha = 1)...")
-
-pvals_hpgsea_1 <- map_dbl(seq_len(n_datasets), function(i) {
-  set.seed(i)
-  stats <- rnorm(n_genes)
-  names(stats) <- genes
-
-  hpgsea(
-    stats = stats,
-    gene_sets = gene_set,
-    alpha = 1,
-    seed = i
-  )[["p_value"]]
-}, .progress = TRUE)
-
-
-# FGSEA (alpha = 0) ----
-
-message("FGSEA (alpha = 0)...")
-
-pvals_fgsea_0 <- map_dbl(seq_len(n_datasets), function(i) {
-  set.seed(i)
-  stats <- rnorm(n_genes)
-  names(stats) <- genes
-
-  invisible(
-    capture.output(
-      pval <- fgsea(
-        stats = stats,
-        pathways = gene_set,
-        gseaParam = 0,
-        nproc = 1L
-      )[["pval"]]
-    )
+  message(
+    "inter-gene correlation = ", inter_gene_cor, "..."
   )
 
-  return(pval)
-}, .progress = TRUE)
+  Sigma <- matrix(inter_gene_cor, nrow = set_size, ncol = set_size)
+  diag(Sigma) <- 1
 
+  get_pvals <- function(method) {
 
-# FGSEA (alpha = 1) ----
-
-message("FGSEA (alpha = 1)...")
-
-pvals_fgsea_1 <- map_dbl(seq_len(n_datasets), function(i) {
-  set.seed(i)
-  stats <- rnorm(n_genes)
-  names(stats) <- genes
-
-  invisible(
-    capture.output(
-      pval <- fgsea(
-        stats = stats,
-        pathways = gene_set,
-        gseaParam = 1,
-        nproc = 1L
-      )[["pval"]]
+    gene_set <- list(
+      "set" = genes[seq_len(set_size)]
     )
+
+    pvals <- map_dbl(.x = seq_len(n_datasets), .f = function(i) {
+      set.seed(i)
+
+      if (inter_gene_cor == 0) {
+        stats <- rnorm(n_genes)
+      } else {
+        x <- MASS::mvrnorm(n = 1L, mu = mu, Sigma = Sigma)
+        y <- rnorm(n_genes - set_size)
+        stats <- c(x, y)
+      }
+
+      names(stats) <- genes
+
+      switch(
+        method,
+        hpgsea_0 = {
+          pval <- hpgsea(
+            stats = stats,
+            gene_sets = gene_set,
+            alpha = 0,
+            seed = 0L
+          )[["p_value"]]
+        },
+        hpgsea_1 = {
+          pval <- hpgsea(
+            stats = stats,
+            gene_sets = gene_set,
+            alpha = 1,
+            seed = 0L
+          )[["p_value"]]
+        },
+        ssgsea2_0 = {
+          pval <- ssgsea2(
+            stats = stats,
+            gene_sets = gene_set,
+            alpha = 0,
+            seed = 0L,
+            time_only = FALSE
+          )[["results"]][["p_value"]]
+        },
+        ssgsea2_1 = {
+          pval <- ssgsea2(
+            stats = stats,
+            gene_sets = gene_set,
+            alpha = 1,
+            seed = 0L,
+            time_only = FALSE
+          )[["results"]][["p_value"]]
+        },
+        fgsea_0 = {
+          invisible({
+            capture.output({
+              set.seed(0L)
+              pval <- fgsea(
+                pathways = gene_set,
+                stats = stats,
+                gseaParam = 0,
+                nproc = 1L
+              )[["pval"]]
+            })
+          })
+        },
+        fgsea_1 = {
+          invisible({
+            capture.output({
+              set.seed(0L)
+              pval <- fgsea(
+                pathways = gene_set,
+                stats = stats,
+                gseaParam = 1,
+                nproc = 1L
+              )[["pval"]]
+            })
+          })
+        }
+      )
+
+      return(pval)
+    }, .progress = list(
+      name = method,
+      clear = FALSE,
+      show_after = 0
+    ))
+
+    return(pvals)
+  }
+
+  methods <- c(
+    "hpgsea, 0" = "hpgsea_0",
+    "hpgsea, 1" = "hpgsea_1",
+    "ssGSEA2.0, 0" = "ssgsea2_0",
+    "ssGSEA2.0, 1" = "ssgsea2_1",
+    "fgsea, 0" = "fgsea_0",
+    "fgsea, 1" = "fgsea_1"
   )
 
-  return(pval)
-}, .progress = TRUE)
-
-
-# t-test ----
-
-message("t-test...")
-
-pvals_t <- map_dbl(seq_len(n_datasets), function(i) {
-  set.seed(i)
-  stats <- rnorm(n_genes)
-  x <- stats[seq_len(set_size)]
-  y <- stats[(set_size + 1L):n_genes]
-
-  t.test(x = x, y = y, var.equal = TRUE)[["p.value"]]
-}, .progress = TRUE)
-
-
-# Wilcox test ----
-
-message("Wilcox test...")
-
-pvals_wilcox <- map_dbl(seq_len(n_datasets), function(i) {
-  set.seed(i)
-  stats <- rnorm(n_genes)
-  x <- stats[seq_len(set_size)]
-  y <- stats[(set_size + 1L):n_genes]
-
-  wilcox.test(x = x, y = y)[["p.value"]]
-}, .progress = TRUE)
-
-
-res <- list(
-  "HPGSEA, 0" = pvals_hpgsea_0,
-  "HPGSEA, 1" = pvals_hpgsea_1,
-  "ssGSEA2.0, 0" = pvals_ssgsea2_0,
-  "ssGSEA2.0, 1" = pvals_ssgsea2_1,
-  "FGSEA, 0" = pvals_fgsea_0,
-  "FGSEA, 1" = pvals_fgsea_1,
-  "Wilcox's rank sum test, -" = pvals_wilcox,
-  "t-test, -" = pvals_t
-) %>%
-  tibble::enframe(
-    name = "method",
-    value = "pval"
-  ) %>%
-  tidyr::separate_wider_delim(
-    cols = method,
-    delim = ", ",
-    names = c("method", "alpha")
-  ) %>%
-  mutate(
-    across(
-      .cols = c(
+  res <- lapply(unname(methods), get_pvals) %>%
+    setNames(
+      names(methods)
+    ) %>%
+    tibble::enframe(
+      name = "method",
+      value = "pval"
+    ) %>%
+    tidyr::separate_wider_delim(
+      cols = method,
+      delim = ", ",
+      names = c("method", "alpha")
+    ) %>%
+    mutate(
+      across(
+        .cols = c(
+          method,
+          alpha
+        ),
+        .fns = ~ factor(
+          x = .x,
+          levels = unique(.x)
+        )
+      )
+    ) %>%
+    tidyr::unnest(
+      pval
+    ) %>%
+    summarise(
+      .by = c(
         method,
         alpha
       ),
-      .fns = ~ factor(
-        x = .x,
-        levels = unique(.x)
+      p0.01 = mean(pval < 0.01),
+      p0.02 = mean(pval < 0.02),
+      p0.05 = mean(pval < 0.05),
+      p0.10 = mean(pval < 0.10)
+    )
+
+  if (inter_gene_cor == 0) {
+    saveRDS(
+      res,
+      file = file.path(
+        "simulation",
+        "data",
+        "pvalues_type_I_error.rds"
       )
     )
-  ) %>%
-  tidyr::unnest(
-    pval
-  ) %>%
-  summarise(
-    .by = c(
-      method,
-      alpha
-    ),
-    p0.01 = mean(pval < 0.01),
-    p0.02 = mean(pval < 0.02),
-    p0.05 = mean(pval < 0.05),
-    p0.10 = mean(pval < 0.10)
-  )
+  } else {
+    saveRDS(
+      res,
+      file = file.path(
+        "simulation",
+        "data",
+        "pvalues_inter_gene_correlation.rds"
+      )
+    )
+  }
 
-saveRDS(
-  res,
-  file = file.path(
-    "simulation",
-    "data",
-    "pvalues_type_I_error.rds"
-  )
-)
+}
